6ad7be01b919f64aa7bf3155058c24e83adc9b9a
Zaynaib Giwa <zaynaibg@gmail.com>
2019-06-19 07:34:42 -0500

    debugged all pairwise functions

--- coursera-algorithms/solutions/week1/pairwise.py
+++ coursera-algorithms/solutions/week1/pairwise.py
@@ -4,8 +4,7 @@
 Resources: https://stackoverflow.com/questions/126524/iterate-a-list-with-indexes-in-python
 '''
 
-import random
-
+from random import randint
 '''
 Original pairwise product algorithm
 Slowest run time
@@ -25,23 +24,30 @@ def max_pairwise_product(numbers):
     Faster pairwise product algorithm
     faster runtime
 '''
+
+
 def pairwise1(numbers):
-    max_index1 = 0
-    max_index2 = 0
-    
+
+    max_index1 =  0
+    max_index2 = 0    
     #find the highest number
     for i, val in enumerate(numbers):
-        if numbers[i] > numbers[max_index1]:
+        if int(numbers[i]) > int(numbers[max_index1]):
             max_index1 = i
-
+    
+    if max_index1 == 0:
+        max_index2 = 1
+    else:
+        max_index2 = 0   
     #find the second highest number
     for j, val in enumerate(numbers):
-        if j != max_index1 and numbers[j] > numbers[max_index2]:
+        if j!=max_index1 and int(numbers[j]) > int(numbers[max_index2]):
             max_index2 = j
+    
+        
     
     #print(max_index1)    
     #print(max_index2)
-
     return int(numbers[max_index1]) * int(numbers[max_index2])    
   
 
@@ -50,35 +56,49 @@ def pairwise1(numbers):
     Another solution to the pairwise product 
     Even faster run time (probably)
 '''
+
+
+
 def pairwise(arr):
     arr.sort(reverse = True)
     solution = int(arr[0]) * int(arr[1])
     return solution
 
 
-def stressTess(n,m):
-    arr = []
-    while true:
+def stressTest():
+   while True:
+        arr = []
         for x in range(5):
-            arr.append(x)
+            random_num = randint(2,101)
+            arr.append(random_num)
+        print(arr)
+        print('####')
+        result1 = max_pairwise_product(arr)
+        result2 = pairwise1(arr)
+        print("Result 1 {}, Result2 {}".format(result1,result2))
 
-'''
-
-StressTest(N,M): 
-while true: n←random integer between 2 and N allocate array A[1...n] 
-    for i from 1 to n: A[i]←random integer between 0 and M print(A[1...n])
-         result1←MaxPairwiseProductNaive(A) 
-        result2←MaxPairwiseProductFast(A) 
-if result1 =result2: print(“OK”) else: print(“Wrong answer: ”, result1, result2) return
+        if result1 != result2:
+            print("wrong answer: {} **** {}".format(result1, result2))
+            break
+        else:
+            print("############################################# \n Ok", result1, result2)
 
 
-'''
+        
+
 
 
 
 if __name__ == '__main__':
+    #stressTest()
+
+    
     length = input()
-    numbers = input().split(' ')
-    #answer = pairwise(numbers)
+    numbers = [int(x) for x in input().split()]
+    answer = pairwise(numbers)
     answer = pairwise1(numbers)
     print(answer)
+    
+
+
+    
