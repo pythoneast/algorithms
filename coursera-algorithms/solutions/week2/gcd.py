a4e41b9bb1b9bbb3815b84bf5822464266d90332
Zaynaib Giwa <zaynaibg@gmail.com>
2019-06-24 08:16:34 -0500

    used the euclidean algorithm to find the gcd

---
+++ coursera-algorithms/solutions/week2/gcd.py
@@ -0,0 +1,45 @@
+#python3
+import sys
+
+def gcd_naive(a, b):
+    current_gcd = 1
+    for d in range(2, min(a, b) + 1):
+        if a % d == 0 and b % d == 0:
+            if d > current_gcd:
+                current_gcd = d
+
+    return current_gcd
+
+'''
+a prime is the remainder when a/b
+#eculidean algorithm for gcd visit youtube
+'''
+def gcd_fast(a,b):
+    factors = []
+    while a != 0:
+        remainder = b % a
+        b = a
+        a = remainder
+        #print(a)
+        factors.append(a)
+        #print(factors)
+        
+    return factors[-2]
+    
+
+def gcd2(a,b):
+    factors = []
+    while a != 0:
+        remainder = b % a
+        b = a
+        a = remainder
+        #print(a)
+        factors.append(a)
+        #print(factors)
+        
+    return factors[-2]
+
+if __name__ == "__main__":
+    print(gcd_fast(18,35))
+    print(gcd_fast(28851538 ,1183019 ))
+    
