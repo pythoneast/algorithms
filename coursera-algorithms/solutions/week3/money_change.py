d281710564578334b95e4f0e7838501eff23ddb0
Zaynaib Giwa <zaynaibg@gmail.com>
2019-06-25 12:08:09 -0500

    created a faster algorithm for change problem runs n^2 instead of n^3

--- coursera-algorithms/solutions/week3/money_change.py
+++ coursera-algorithms/solutions/week3/money_change.py
@@ -4,56 +4,44 @@ import sys
 '''
     Naive Algorithim 
 '''
+
+#BigO n^3
 def get_change(m):
     #write your code here
     total_counter = 0
-    change_counter = {"ten":0,"five":0,"one":0}
     choices = [10,5,1] 
+
     while m - choices[0] >=0:
-        print(10)
+        #print(10)
         m = m -10
+        total_counter +=1
+
     while m - choices[1] >= 0:
-        print(5)
+        #print(5)
         m = m -5
+        total_counter +=1
     while m - choices[2] >= 0:
-        print(1)
+        #print(1)
         m = m -1
-    
-    '''
-    for i in choices:
-        if m - i >=0: 
-            m = m - i
-            print(i)
-            print(m,"###sum##")
-    '''
-    
-    '''
-    while m  > 0:
-        if m -10 > 0:
-            print(10)
-            m  = m - 10
-            change_counter["ten"] +=1
-            total_counter +=1
-        
-        if m - 5 >= 0:
-            print(5)
-            m = m - 5
-            change_counter["five"]+=1
-            total_counter +=1
-        
-        if m - 1 >= 0:
-            print(1)
-            m = m - 1
-            change_counter["one"]+=1
-            total_counter +=1
+        total_counter +=1
 
+    return total_counter
 
-    '''
+#Big O n^2
+def get_change_fast(m):
+    total_counter = 0
+    choices = [10,5,1] 
 
-    return None
+    for i in range(3):
+        while m - choices[i]>=0:
+            #print(choices[i])
+            m -=choices[i]
+            total_counter += 1
+            
+    return total_counter
 
 if __name__ == '__main__':
     number = int(input())
-    print(get_change(number))
+    print(get_change_fast(number))
 
 
