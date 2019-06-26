04f79da4a265edffcb2e3d8da7ea5ec1731e07b9
Zaynaib Giwa <zaynaibg@gmail.com>
2019-06-25 21:47:00 -0500

    try to fit item with maximum value into loot then find the second largest item

--- coursera-algorithms/solutions/week3/loot.py
+++ coursera-algorithms/solutions/week3/loot.py
@@ -1,22 +1,49 @@
 #python3
 import sys
 
-def get_optimal_value(capacity, weights, values):
-    value = 0
+def get_optimal_value(capacity,values, weights):
+
+
+    val_item = []
+    max_index = 0
+    value_knap = 0
+
+    #calculate the value of each item
+    for i in range(len(weights)):
+        value = values[i]/weights[i]
+        val_item.append(value)
+
+    #determine which value is bigger
+        if value > max_index:
+            max_index = i
+    print(max_index, "max index")
+
+    #figure out how much weight of the most value items you can use
+    if capacity > 0:
+        value_knap += values[max_index]
+        capacity -= weights[max_index]
+    else:
+        #remove from greatest value from array index and use the second largest value
+        values.pop(max_index)
+        weights.pop(max_index)
+    
+    print(value_knap)
+
+
     # write your code here
     #1 10 
     #500 30
+    return val_item
 
-        
+'''
     z = weights/values
     if values > capacity:
         z *= capacity
-
-    return z
+'''
 
 
 if __name__ == "__main__":
-    print(get_optimal_value(10,500,30))
+    print(get_optimal_value(50,[60,100,120],[20,50,30]))
     #print(500/30)
     '''
     n, capacity = list(map(int,input().split(' ')))
