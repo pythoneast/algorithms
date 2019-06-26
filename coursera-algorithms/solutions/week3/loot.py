ad6b15bd413ba646d811ddd6fdbaa4f09afaeaed
Zaynaib Giwa <zaynaibg@gmail.com>
2019-06-25 21:09:07 -0500

    created solution for only one sceranrio

--- coursera-algorithms/solutions/week3/loot.py
+++ coursera-algorithms/solutions/week3/loot.py
@@ -6,20 +6,27 @@ def get_optimal_value(capacity, weights, values):
     # write your code here
     #1 10 
     #500 30
-    z = value/weights
 
-    return value
+        
+    z = weights/values
+    if values > capacity:
+        z *= capacity
+
+    return z
 
 
 if __name__ == "__main__":
-    
+    print(get_optimal_value(10,500,30))
+    #print(500/30)
+    '''
     n, capacity = list(map(int,input().split(' ')))
     print(n)
     while n > 0:
+
         #get_optimal_value(n,5,20)
         n -=1
         print(n)
-
+    '''
 
     '''
     data = list(map(int, sys.stdin.read().split()))
