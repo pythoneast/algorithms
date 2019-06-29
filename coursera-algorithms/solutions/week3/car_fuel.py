d4bcd2e6e8073942ff5a1b4a22f109857af031f9
Zaynaib Giwa <zaynaibg@gmail.com>
2019-06-29 14:17:24 -0500

    basic loot

--- coursera-algorithms/solutions/week3/car_fuel.py
+++ coursera-algorithms/solutions/week3/car_fuel.py
@@ -7,37 +7,32 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     # write your code heregi
-    stops.sort(reverse=True)
-    num_stops = 0
+    n = len(stops)
     first_stop = 0
     max_index = 0
-    #whats the further stop you can go?
-    for i, stop in enumerate(stops):
-        if tank - stop > 0:
-            first_stop = stop
+
+
+  
+
+
+    for i in range(n):
+        #find the maximum first stop
+        if tank - stops[i] > 0:
             max_index = i
-            num_stops +=1
-            break
-    print(first_stop)
+        #can i reach my next gas station
+        while tank  - stops[i ] > 0:
+            print('hello')
+            #can I reach my next destination?
+
+
+        print(stops[max_index])
     
-    stops = stops[:max_index]
-    for i, stop in enumerate(stops):
-        if stops[i] - first_stop < tank:
-            num_stops +=1
-            first_stop = stop
-            break
-        else:
-            print("too far away")
-    print(stops)
-    print(first_stop)
-    if distance - first_stop < tank:
-        print(num_stops)
+
+
     
-        
-    return -1
 
 if __name__ == '__main__':
-    #print(compute_min_refills(950,400,[200,375,550,750]))
-    print(compute_min_refills(10,3,[1,2,5,9]))
+    print(compute_min_refills(950,400,[200,375,550,750]))
+    #print(compute_min_refills(10,3,[1,2,5,9]))
     #d, m, _, *stops = map(int, sys.stdin.read().split())
     #print(compute_min_refills(d, m, stops))
--- coursera-algorithms/solutions/week3/loot.py
+++ coursera-algorithms/solutions/week3/loot.py
@@ -1,10 +1,14 @@
 #python3
+
 import sys
 
 def get_optimal_value(capacity,values, weights):
 
     #values for each item v/w by index
     val_item = []
+    
+    #greatest value
+    max_value = 0
 
     #the position of the item with the most value
     max_index = 0
@@ -13,75 +17,68 @@ def get_optimal_value(capacity,values, weights):
     value_knap = 0
 
     #while the bag is not full - when capacity is greater 0 
-    while capacity >0:
         #calculate the value of each item
-        for i in range(len(weights)):
-            value = values[i]/weights[i]
-            val_item.append([value,weights[i]])
-
-        #determine which value is bigger
-            if value > max_index:
-                max_index = i
+    for i in range(len(weights)):
+        ratio = values[i]/weights[i]
+        val_item.append([ratio,weights[i]])
         
+        #determine which value is bigger
+        if ratio > max_value:
+            max_value = ratio
+            max_index = i
+        
+    print(max_value,max_index)
+    print(val_item)
+    
+    #if capacity < weight
+    #then take the ratio
+    #update the capcity
 
-        #if the weight of the item cost more than capcity of the knapsack
-        if val_item[max_index][1] > capacity:
-            value_knap += val_item[max_index][0] * capacity
-            capacity -= val_item[max_index][1]
-            #print(value_knap)
+    if capacity < val_item[max_index][1]:
+        capacity = capacity/val_item[max_index][1]
+        print(c_val)
+        
+        
+    #else if capcity >= weight 
+    #take the entire weight
+    #update the capcity
 
-        else:
-            value_knap += val_item[max_index][0] * val_item[max_index][1]
-            capacity -= val_item[max_index][1]
-            val_item.pop(max_index)
+    elif capacity >= val_item[max_index][1]:
+        capacity = capacity - val_item[max_index][1]
+        print(capacity)
+        
+        
+    #update the list of items
+    val_item.pop(max_index)
+    
+    #find the max_index and do the if statement again
 
+    
+    
+    
+    #capacity - weight
+    #update the list of items
+    
         #print("value of knapsack",value_knap)
         #print("how much capcity left", capacity)
         #print(val_item)
 
     return value_knap
+    
+'''
+1 1000
+500 30
+
+
+
+'''
 
 
 
 if __name__ == "__main__":
-    n, cap = list(map(int,input().split(' ')))
-    v = []
-    w = []
-    '''
-    3 50
-     60 20 
-     100 50 
-     120 30
-    '''
-    while n > 0:
 
-        #get_optimal_value(n,5,20)
-        max_input = list(map(int,input().split(' ')))
-        v.append(max_input[0])
-        w.append(max_input[1])
-
-        n -=1
-        #print(v)
-        #print(n)
-    print(get_optimal_value(cap,v,w))
-
-    #print(get_optimal_value(50,[60,100,120],[20,50,30]))
+    print(get_optimal_value(50,[60,100,120],[20,50,30]))
     #print(get_optimal_value(10,[500],[30]))
     #print(500/30)
-    '''
-    print(n)
-    while n > 0:
+   
 
-        #get_optimal_value(n,5,20)
-        n -=1
-        print(n)
-    '''
-
-    '''
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
-    opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
-    '''
