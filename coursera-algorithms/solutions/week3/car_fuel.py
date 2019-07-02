56c9b8b8d94bb61fb6e48a8a6b90f6e0b102f4e5
Zaynaib Giwa <zaynaibg@gmail.com>
2019-07-01 21:23:10 -0500

    naive solution to gas fuel problem

--- coursera-algorithms/solutions/week3/car_fuel.py
+++ coursera-algorithms/solutions/week3/car_fuel.py
@@ -1,10 +1,6 @@
 # python3
 import sys
 
-
-
-
-
 #python3
 def compute_min_refills(distance, tank, stops):
     # write your code heregi
@@ -27,10 +23,46 @@ def compute_min_refills(distance, tank, stops):
 
 
         print(stops[max_index])
-    
 
 
+max_stops = 4
+tank = 400
+stations =[200,375,500,700]
+distance = []
+
+for station in stations:
+    x = 400 - station
+    distance.append([x,station])
+
+
+least = distance[0][0]
+for i,v in enumerate(distance):
+    #least = distance[i][0]
     
+    #this is the first move
+    if least > distance[i][0] and distance[i][0]>0:
+        least = i
+        
+#miles traveled       
+miles = tank + stations[least]
+
+#want to forget about the rest of the distances
+distance = distance[least +1:]
+stations = stations[least+1:]
+for i, v in enumerate(distance):
+    if least > distance[i][0] +miles and distance[i][0]>0:
+        least = i
+print(stations[least], "final")
+
+print(miles, "miles")
+
+'''
+y = 400 + 375
+print(y)
+print(y -100)
+print(y - 300)
+
+'''
 
 if __name__ == '__main__':
     print(compute_min_refills(950,400,[200,375,550,750]))
