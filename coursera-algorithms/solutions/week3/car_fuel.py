43c78e5e21adfcc19e3539fc655ef3285c3ea46b
Zaynaib Giwa <zaynaibg@gmail.com>
2019-07-03 21:30:06 -0500

    delete old car_fuel file for correct verison of algorithm

--- coursera-algorithms/solutions/week3/car_fuel.py
+++ coursera-algorithms/solutions/week3/car_fuel.py
@@ -1,63 +1,48 @@
-# python3
-import sys
+#python 3
 
-max_stops = 4
-tank = 400
-stations =[200,375,500,700]
-distance = []
-
-
-
-
-for station in stations:
-    x = 400 - station
-    distance.append([x,station])
-    
-print(distance)
-
-least = distance[0][0]
-
-for i,v in enumerate(distance):
-    #least = distance[i][0]
-    
-    #this is the first move
-    if least > distance[i][0] and distance[i][0]>0:
-        least = i
-        
-#miles traveled       
-miles = tank + stations[least]
-
-#want to forget about the rest of the distances
-distance = distance[least +1:]
-stations = stations[least+1:]
-
-for i, v in enumerate(distance):
-    if least > distance[i][0] +miles and distance[i][0]>0:
-        least = i
-print(stations[least], "final")
-
-print(miles, "miles")
 
 def compute_min_refills(distance, tank, stops):
-        d_travel = 0
-        smallest = 400 - distance[0]
-        for station in distance:
-                miles = 400 - station
-                if smallest > miles and miles > 0:
-                        print(stations)
 
+    num_stops = 0
+    n = len(stops)
+    while n > 0:
+        smallest = tank - stops[0]
+
+        for i,v in enumerate(stops):
+            
+            miles = tank - stops[i]
         
+            if miles > 0 and smallest > miles:
+                smallest = miles
+                d_travel = stops[i] + tank
+                num_stops += 1
+        
+        stops = stops[i-1:]
+        smallest = d_travel - stops[0]
+        for i,v in enumerate(stops):
+            #print(d_travel - stops[i])
+            if d_travel - stops[i] < 0:
+                return -1
+                break
+            
+            elif d_travel - stops[i] < tank and smallest > d_travel - stops[i]:
+                #print(stops[i])
+                num_stops += 1
+            
+        #print(stops)
+        stops = stops[i:]
+        #print(stops)
+        if len(stops) == 1:
+            #print(num_stops)
+            return num_stops
+            break
 
-'''
-y = 400 + 375
-print(y)
-print(y -100)
-print(y - 300)
-
-'''
-
+  
+        n -= 1
+   
 if __name__ == '__main__':
     print(compute_min_refills(950,400,[200,375,550,750]))
-    #print(compute_min_refills(10,3,[1,2,5,9]))
+    print(compute_min_refills(10,3,[1,2,5,9]))
     #d, m, _, *stops = map(int, sys.stdin.read().split())
     #print(compute_min_refills(d, m, stops))
+
